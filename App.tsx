346324809547add151b70d57679d98103452e5c4
Mounir Abcire <mounirwebdevcode@gmail.com>
2025-05-06 01:27:01 +0100

    feat: finish all todo list app logic and styling

--- App.tsx
+++ App.tsx
@@ -1,22 +1,35 @@
-import {
-    StyleSheet,
-    View,
-    Text,
-} from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, StatusBar } from "react-native";
+
+import TodoInput from "./components/todos/TodoInput";
+import TodoList from "./components/todos/TodoList";
 
 function App() {
+    const [todos, setTodos] = useState<string[]>([]);
+
+    const deleteTodoItem = (itemID: number) => {
+        setTodos((prev) => prev.filter((item, index) => index !== itemID));
+    };
+
     return (
         <View style={styles.container}>
-            <Text>Hello World</Text>
+            <TodoInput onSetTodos={setTodos} />
+            <TodoList todos={todos} onDeleteTodoItem={deleteTodoItem}/>
+            <StatusBar barStyle="dark-content" />
         </View>
     );
 }
 
+// FFFFFF
+// 383D3B => black
+// 623CEA => purple
+
 const styles = StyleSheet.create({
     container: {
+        paddingTop: 80,
+        paddingInline: 20,
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        gap: 56,
         backgroundColor: "#fff",
     },
 });
